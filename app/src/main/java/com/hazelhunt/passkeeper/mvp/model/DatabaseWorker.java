620cfe1fed0fa1a8cd6d2285b66ab5bb25bb1896
Ninise <nikitonze@hotmail.com>
2016-05-01 17:35:47 +0300

    Class for work with db added

--- app/src/main/java/com/hazelhunt/passkeeper/mvp/model/DatabaseWorker.java
+++ app/src/main/java/com/hazelhunt/passkeeper/mvp/model/DatabaseWorker.java
@@ -1,7 +1,54 @@
 package com.hazelhunt.passkeeper.mvp.model;
 
-/**
- * Created by ninise on 01.05.16.
- */
+import android.content.Context;
+
+import rx.Observable;
+import rxsqlite.RxSQLite;
+import rxsqlite.RxSQLiteClient;
+import rxsqlite.RxSQLiteWhere;
+
 public class DatabaseWorker {
+
+    private static final String TABLE_ACCS = "acc_table";
+
+    private static final String DATABASE_NAME = "accs";
+    private static final String KEY_ID = "_id";
+    private static final String KEY_URL = "mUrl";
+    private static final String KEY_LOGIN = "mLogin";
+    private static final String KEY_PASS = "mPass";
+    private static final String KEY_EMAIL = "mEmail";
+    private static final String KEY_EXTRA = "mExtra";
+
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_ACCS +
+            "(" + KEY_ID + " INTEGER PRIMARY KEY," +
+            KEY_URL + " TEXT," +
+            KEY_LOGIN + " TEXT," +
+            KEY_PASS + " TEXT," +
+            KEY_EMAIL + " TEXT," +
+            KEY_EXTRA + " TEXT" + ")";
+
+    public static void register(Context context) {
+        RxSQLite.register(RxSQLiteClient.builder(context, 1)
+                .doOnOpen(db -> db.exec("PRAGMA case_sensitive_like = true;"))
+                .doOnUpgrade((sqLiteDb, integer, integer2) -> sqLiteDb.exec("DROP TABLE IF EXISTS " + TABLE_ACCS))
+                .doOnCreate(db -> db.exec(CREATE_TABLE))
+                .enableTracing()
+                .build());
+    }
+
+    public static Observable<PassModel> save(PassModel model) {
+            return RxSQLite.save(model);
+    }
+
+    public static Observable<PassModel> load(long id) {
+        return RxSQLite.query(PassModel.class, new RxSQLiteWhere().where(KEY_ID + " = " + id));
+    }
+
+    public static Observable<PassModel> loadAll() {
+        return RxSQLite.query(PassModel.class, new RxSQLiteWhere());
+    }
+
+    public static Observable<Integer> remove(PassModel model) {
+        return RxSQLite.remove(model);
+    }
 }
